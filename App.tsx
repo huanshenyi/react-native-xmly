0a5c802a85be70897f039a152ccce2ad59ed87e7
huanshenyi <txy1226052@gmail.com>
2020-11-26 22:53:34 +0900

    多環境対応プラグイン追加

--- App.tsx
+++ App.tsx
@@ -19,12 +19,10 @@ import {
 } from 'react-native';
 
 import {
-  Header,
-  LearnMoreLinks,
   Colors,
-  DebugInstructions,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
+import Config from "react-native-config";
 
 declare const global: {HermesInternal: null | {}};
 
@@ -36,40 +34,9 @@ const App = () => {
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
+           <View>
+             <Text>{ Config.API_URL }</Text>
+           </View>
         </ScrollView>
       </SafeAreaView>
     </>
--- README.md
+++ README.md
@@ -1,3 +1,21 @@
+# typescript 使用のテンプレート
+
 ```
 npx react-native init ximalaya --template react-native-template-typescript
+```
+
+# セットアップ
+
+```
+yarn install
+// iso
+cd ios & pod install
+
+// android
+```
+
+# 使用ライブラリ
+
+```
+react-native-config https://js.coach/package/react-native-config (多環境対応)
 ```
